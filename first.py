01806e1a6b4efaa455f991a73520839df88709f0
Mr1ter <79842005+Mr1ter@users.noreply.github.com>
2021-03-05 21:30:42 +0300

    Update first.py

--- first.py
+++ first.py
@@ -49,7 +49,7 @@ async def roll(ctx, arg):
 
 @bot.command()
 async def help(ctx):
-	embed = discord.Embed(color = 0xff0000, title = 'Команды', description='Префикс бота - !? \nroll - кинуть кубик \nr - ответ на вопрос (Да/Нет) \nfunny - :) \nsay - скажет за Вас всё что угодно! \nsayemb Шрифт(Жирный/Тонкий) текст - текст в ембед \ninvite - ссылка приглашение бота на свой сервер! \ninfo - инфо о боте \nban - забанить участника \nkick - кикнуть участника')
+	embed = discord.Embed(color = 0xff0000, title = 'Команды', description='Префикс бота - !? \nroll - кинуть кубик \nr - ответ на вопрос (Да/Нет) \nfunny - :) \ninvite - ссылка приглашение бота на свой сервер! \ninfo - инфо о боте \nban - забанить участника \nkick - кикнуть участника')
 	await ctx.send(embed = embed)
 
 @bot.command()
