ba66748dde9e673e15181489fabff2413ca1a161
Mr1ter <79842005+Mr1ter@users.noreply.github.com>
2021-03-14 23:07:13 +0300

    Update first.py

--- first.py
+++ first.py
@@ -53,7 +53,7 @@ async def funny(ctx):
 @bot.command()
 async def roll(ctx, arg=None):
 	if arg==None:
-		await ctx.send("Вы не ввели число.")
+		await ctx.send(embed= discord.Embed(color = 0xff0000, title = 'У-упс...', description='Введите число.'))
 	else:
 		author = ctx.message.author
 		B=int(arg)
